548cea1b186f006ee5b7d095b60196be379833c9
luapbbor <luapbbor@gmail.com>
2020-12-07 06:07:58 +1100

    Initial Project Setup

---
+++ index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>candles</title>
+    <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+
+<h4 id="grid" class="headline-secondary">Grid System</h4>
+
+  <div class="row">
+    <div class="col-12 theme">.col-12</div>
+  </div>
+  <div class="row">
+    <div class="col-6 theme">.col-6</div>
+    <div class="col-6 theme">.col-6</div>
+  </div>
+  <div class="row">
+    <div class="col-4 theme">.col-4</div>
+    <div class="col-4 theme">.col-4</div>
+    <div class="col-4 theme">.col-4</div>
+  </div>
+  <div class="row">
+    <div class="col-3 theme">.col-3</div>
+    <div class="col-3 theme">.col-3</div>
+    <div class="col-3 theme">.col-3</div>
+    <div class="col-3 theme">.col-3</div>
+  </div>
+  <div class="row">
+    <div class="col-5 theme">.col-5</div>
+    <div class="col-7 theme">.col-7</div>
+  </div>
+  <div class="row">
+    <div class="col-8 theme">.col-8</div>
+    <div class="col-4 theme">.col-4</div>
+  </div>
+  <div class="row">
+    <div class="col-7 theme centered">.centered .col-7</div>
+  </div>
+
+    
+</body>
+</html>
